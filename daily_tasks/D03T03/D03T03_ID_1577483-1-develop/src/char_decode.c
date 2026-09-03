1a3ffd1f6767f9765ebaa8dec97ceed0ae164749
LilitNefara <140747899+LilitNefara@users.noreply.github.com>
2026-09-03 21:05:29 +0300

    upd 03.09pool char_decode.c

--- daily_tasks/D03T03/D03T03_ID_1577483-1-develop/src/char_decode.c
+++ daily_tasks/D03T03/D03T03_ID_1577483-1-develop/src/char_decode.c
@@ -1,20 +1,58 @@
 #include <stdio.h>
 
-int main(int a, char* argv[]) {
-        if (a < 1) return printf("%d", a);
-        switch (*argv[1]) {
-        case '0': {
-            char x1, x2, x3, x4, x5;
-            scanf("%c %c %c %c %c", &x1, &x2, &x3, &x4, &x5);
-            printf("%x %x %x %x %x", x1, x2, x3, x4, x5);
-            break;
-        }
-        case '1': {
-            int w, b, c, d, e;
-            scanf("%x %x %x %x %x", &w, &b, &c, &d, &e);
-            printf("%c %c %c %c %c", (char)w, (char)b, (char)c, (char)d, (char)e);
-            break;
-        }
+void code();
+void decode();
+
+int main(int argc, char **argv) {
+    int error = 0;
+
+    if (argc != 2) {
+        printf("n/a");
+        error = 1;
     }
+    argv++;
+
+    char mode = **argv;
+    char next_char = *(*argv + 1);
+
+    if ((mode != '0' && mode != '1') || next_char != '\0') error = 1;
+
+    if (error == 0) {
+        if (mode == '0') {
+            code();
+        }
+        
+        // if (argv[1][0] == '0' && argv[1][1] == '\0') {
+            
+        // }
+    //      else if (argv[1][0] == '1' && argv[1][1] == '\0') {
+    //     int w, b, c, d, e;
+    //     if (scanf(" %x %x %x %x %x", &w, &b, &c, &d, &e) == 5) printf("%c %c %c %c %c", (char)w, (char)b, (char)c, (char)d, (char)e);
+    //     else error = 1;
+    // } else error = 1;
+    }
+    if (error == 1) printf("n/a");
     return 0;
     }
+
+// void code() {                                                                                    mandarin_version
+//     char x, space_check;
+//     while (scanf(" %c%c", &x, &space_check) >= 1) {
+//         if (space_check == '\n') {
+//             printf("%X", x);
+//             break;
+//         }
+//         if (space_check == ' ') printf("%X ", x);
+//     }
+// }
+
+void code() {
+    char x, space_check = ' ', go = 's';
+    while (go == 's') {
+        if (scanf(" %c%c", &x, &space_check) == 2 && space_check == ' ') printf("%X ", x);
+        if (space_check == '\n') {
+            go = 'b';
+            printf("%X", x);
+        }
+    }
+}
